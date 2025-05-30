2e518cb397c7897e9abef795e86ab992d46bb1d4
jadensanares <sanaresj@gmail.com>
2025-05-30 14:44:43 +0800

    finsihed the class to represent as the quiz questions converted to OOP structure

--- question.py
+++ question.py
@@ -6,4 +6,10 @@ class Question:
         self.choices = choices
         self.correct = correct_answer
     
-    
+    def display(self):
+        print("\n" + self.question_text)
+        for key in sorted(self.choices):
+            print(f"{key}: {self.choices[key]}")
+
+    def check_answer(self, user_answer):
+        return user_answer == self.correct_answer
