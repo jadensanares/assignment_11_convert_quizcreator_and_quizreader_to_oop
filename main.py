d68e03718bcfa336facb98ff38497dcd44ad22d2
jadensanares <sanaresj@gmail.com>
2025-05-30 18:18:47 +0800

    i've added the full functiuons for the main user interface, however when i tested the program, there are errors and the quiz is not loading, checking the problem

--- main.py
+++ main.py
@@ -5,3 +5,17 @@ def main():
     print("Welcome to this simple Quiz Program!")
     print("1. Create a Quiz")
     print("2. Play the Quiz")
+
+    choice = input("Choose a number option (1 or 2): ")
+
+    if choice == "1":
+        creator = QuizCreator()
+        creator.run()
+    elif choice == "2":
+        game = QuizGame()
+        game.start_quiz()
+    else:
+        print("Invalid choice. Please run the program again and specifically select 1 or 2.")
+    
+if __name__ == "__main__":
+    main()
