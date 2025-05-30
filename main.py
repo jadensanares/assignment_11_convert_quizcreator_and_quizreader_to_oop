95357d52ad88e87e8e637c78d3005b39f950a29d
jadensanares <sanaresj@gmail.com>
2025-05-30 18:15:01 +0800

    imported the class quizcreator and quizgame

---
+++ main.py
@@ -0,0 +1,7 @@
+from quiz_creator import QuizCreator
+from quiz_game import QuizGame
+
+def main():
+    print("Welcome to this simple Quiz Program!")
+    print("1. Create a Quiz")
+    print("2. Play the Quiz")
